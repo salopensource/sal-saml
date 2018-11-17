4df0abd94c47d92e3442a8575bfbfc63d3b6a0f2
Graham Gilbert <graham@grahangilbert.com>
2018-11-17 22:12:14 +0000

    Script for manual builds

---
+++ trigger_build.sh
@@ -0,0 +1,15 @@
+source SAL_ENVS
+
+if [ "$1" != "" ]; then
+    BRANCH="$1"
+else
+    BRANCH="master"
+fi
+
+URL="https://circleci.com/api/v1.1/project/github/salopensource/sal-saml/tree/${BRANCH}"
+
+jq -n '{TAG: "latest"}' | curl -X POST -d @- \
+  --user ${CIRCLE_API_USER_TOKEN}: \
+  --url $URL \
+  -H 'Content-Type: application/json' \
+  -H 'Accept: application/json'
